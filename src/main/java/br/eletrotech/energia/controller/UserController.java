192e26b99566b97abe63499c51e41ee33d967e49
ChristianVarela <christian_varela@outlook.com.br>
2023-03-28 23:42:51 -0300

    fix: user registration task started

--- src/main/java/br/eletrotech/energia/controller/UserController.java
+++ src/main/java/br/eletrotech/energia/controller/UserController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.eletrotech.energia.dto.SignUpDTO;
@@ -25,7 +27,7 @@ public class UserController {
 	}
 	
 	@PostMapping
-	public void createUser(SignUpDTO dto) {
+	public void createUser(@RequestBody SignUpDTO dto) {
 		System.out.println();
 	}
 }
